411bb9d60e121ebab31c19f627498bfc2b1aeec6
realTobby <tkattanek00@gmail.com>
2022-02-05 15:39:51 +0100

    Submission ready

--- Assets/Scripts/Bird/BirdFly.cs
+++ Assets/Scripts/Bird/BirdFly.cs
@@ -2,9 +2,28 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public enum BirdState
+{
+
+    Flying,
+    Ragdoll
+}
 
 public class BirdFly : MonoBehaviour
 {
+    [SerializeField] Text timeText;
+
+    public int MaxPoints = 7;
+
+    public int Points = 0;
+
+    [SerializeField] GameObject myRagdollPrefab;
+
+    [SerializeField] BirdState currentBirdState;
+
     Rigidbody birdRigidbody;
 
     [SerializeField] float speed;
@@ -22,6 +41,10 @@ public class BirdFly : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Points = 0;
+
+        currentBirdState = BirdState.Flying;
+
         birdRigidbody = this.GetComponent<Rigidbody>();
 
         birdAnimator = this.GetComponent<Animator>();
@@ -40,14 +63,25 @@ public class BirdFly : MonoBehaviour
     public float lookSpeed = 2.0f;
     public float lookXLimit = 45.0f;
 
+    public float CurrentTime = 0;
+
     // Update is called once per frame
     void Update()
     {
+        CurrentTime += Time.deltaTime;
+        timeText.text = (CurrentTime % 60).ToString("F2");
+
+
         birdAnimator.speed = currentSpeed/2000;
 
         HandleMouseInput();
 
         LetBirdFly();
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
 
     }
 
@@ -67,14 +101,14 @@ public class BirdFly : MonoBehaviour
         this.transform.localRotation = Quaternion.Euler(rotationX, rotationY, 0);
 
         // offset camera
-        cameraTransposer.m_FollowOffset = new Vector3(0, rotationX/10, -10);
+        cameraTransposer.m_FollowOffset = new Vector3(0, rotationX/4, -10);
     }
 
     void HandleMouseInput()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            currentSpeed = speed * 2 + 5;
+            currentSpeed = speed * 4;
         }
 
         if(Input.GetMouseButtonUp(0))
@@ -82,5 +116,24 @@ public class BirdFly : MonoBehaviour
             currentSpeed = speed;
         }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.CompareTag("obstacle"))
+        {
+            // enter ragdoll mode
+            var rag = Instantiate(myRagdollPrefab, this.transform.position, Quaternion.identity);
+
+            virtualCamera.LookAt = rag.transform;
+            virtualCamera.Follow = rag.transform;
+
+            cameraTransposer.m_FollowOffset = new Vector3(0, 5 , -10);
+
+            Destroy(this.gameObject);
+
+        }
+
+    }
+
 
 }
--- Assets/Scripts/EarnPoint.cs
+++ Assets/Scripts/EarnPoint.cs
@@ -1,11 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EarnPoint : MonoBehaviour
 {
+    public bool IsTriggered = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(this.gameObject);
+        if(this.transform.CompareTag("finish"))
+        {
+            if(other.gameObject.GetComponent<BirdFly>().Points >= 6)
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+        else
+        {
+            if (IsTriggered == false)
+            {
+                other.gameObject.GetComponent<AudioSource>().Play();
+                other.gameObject.GetComponent<BirdFly>().Points += 1;
+                IsTriggered = true;
+                Destroy(this.gameObject);
+            }
+        }
+
+        
+        
     }
 }
